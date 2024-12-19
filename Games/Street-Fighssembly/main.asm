03e3fdca7698bfd913b4915e7ae53d272faa9431
FlouffyDaWolf <evanmideo@hotmail.com>
2024-12-19 15:15:01 +0100

    own file for input management + fix call never returned

--- Games/Street-Fighssembly/main.asm
+++ Games/Street-Fighssembly/main.asm
@@ -37,44 +37,7 @@ cyclePixelTest:
     mov ax, [ypos]  ; y
     mov bh, [color]  ; color
     call setPixelValue
-    call readKeyboard
-    jmp cyclePixelTest
-
-; -----------------------------------------------
-readKeyboard:
-    ; Read next key in buffer:
-    mov ah, 00h 
-    int 16h
-
-    cmp ah, 4Bh     ; left arrow key 
-    je left
-    cmp ah, 4Dh     ; right arrow key 
-    je right
-    cmp ah, 48h     ; up arrow key    
-    je up 
-    cmp ah, 50h     ; down arrow key
-    je down
-    ; Compare the input with ESCAPE
-    cmp ah, 01h
-    jne cyclePixelTest ; Loop while not "escape".
-
-    jmp exitProgram
-
-up:
-    sub byte [ypos], 10
-    jmp cyclePixelTest
-
-left:
-    sub byte [xpos], 10
-    jmp cyclePixelTest
-
-right:
-    add byte [xpos], 10
-    jmp cyclePixelTest
-
-down:
-    add byte [ypos], 10
-    jmp cyclePixelTest
+    jmp readKeyboard
 
 ; CLEAR SCREEN ---------------------------------------------------------
 clearScreen:
@@ -89,3 +52,6 @@ exitProgram:
     mov ah, 4Ch
     xor al, al
     int 21h
+
+
+%include "KeyboardInput.inc"
