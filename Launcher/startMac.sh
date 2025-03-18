90702d2a69a7cdfeb9a5f27414ecb0f220fadd2d
EnzoGuillouche <145991192+EnzoGuillouche@users.noreply.github.com>
2025-03-19 00:24:55 +0100

    Added first steps for the launcher

--- Launcher/startMac.sh
+++ Launcher/startMac.sh
@@ -4,13 +4,13 @@ clear
 ROOT_DIR="$(dirname "$(realpath "$0")")"
 ROOT_DIR="$(dirname "$ROOT_DIR")"
 NASM="/opt/homebrew/bin/nasm"
-GAME_DIR="$ROOT_DIR/Street-Fighssembly"
+GAME_DIR="$ROOT_DIR/Launcher"
 GAME_BIN="$GAME_DIR/Bin"
 DOSBOX_BIN="/Users/$USER/Desktop/dosbox.app/Contents/MacOS/DOSBox"
 DOSBOX_CONF="/Users/$USER/Library/Preferences/DOSBox 0.74-3-3 Preferences"
 
 # Assemble the game
-"$NASM" "$GAME_DIR/main.asm" -f bin -o "$GAME_BIN/SF.com"
+"$NASM" "$GAME_DIR/launcher.asm" -f bin -o "$GAME_BIN/launcher.com"
 
 # Run the game with the explicit config
-"$DOSBOX_BIN" -c "MOUNT c $GAME_BIN" -c "c:" -c "SF.com" -c "exit"
+"$DOSBOX_BIN" -c "MOUNT c $GAME_BIN" -c "c:" -c "launcher.com" -c "exit"
--- Launcher/startPi.sh
+++ Launcher/startPi.sh
@@ -4,13 +4,13 @@ clear
 ROOT_DIR="$(dirname "$(realpath "$0")")"
 ROOT_DIR="$(dirname "$ROOT_DIR")"
 NASM="nasm"  # Assumes NASM is installed globally
-GAME_DIR="$ROOT_DIR/Street-Fighssembly"
+GAME_DIR="$ROOT_DIR/Launcher"
 GAME_BIN="$GAME_DIR/Bin"
 DOSBOX_BIN="/usr/bin/dosbox"
 DOSBOX_CONF="$HOME/.dosbox/dosbox-0.74.conf"
 
 # Assemble the game
-"$NASM" "$GAME_DIR/main.asm" -f bin -o "$GAME_BIN/SF.com"
+"$NASM" "$GAME_DIR/launcher.asm" -f bin -o "$GAME_BIN/launcher.com"
 
 # Run the game with the explicit config
 "$DOSBOX_BIN" -c "MOUNT c $GAME_BIN" -c "c:" -c "SF.com" -c "exit"
