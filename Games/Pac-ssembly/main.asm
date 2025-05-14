62eff1c0ae96644bfeb798bd60981ee2cef6ba33
EnzoGuillouche <145991192+EnzoGuillouche@users.noreply.github.com>
2025-05-14 10:43:49 +0200

    we go to the next level when we are done with gums (level not displayed :( )

--- Games/Pac-ssembly/main.asm
+++ Games/Pac-ssembly/main.asm
@@ -3,20 +3,31 @@ org 100h
 %include "Sprites/include_sprites.inc"
 %include "Variables/include_variables.inc"
 
-section .data
-    %define FRAME_RATE 30
+%define FRAME_RATE 30
+
+%macro initGhosts 0-* 0, 1, 2, 3
+    %rep %0
+        mov word [ghost_x_speed+%1*2], 0
+        mov word [ghost_y_speed+%1*2], 0
+        mov word [ghost_x_speed_buffer+%1*2], 0
+        mov word [ghost_y_speed_buffer+%1*2], 0
+        mov byte [ghost_movement_buffered+%1], 0
+        %rotate 1
+    %endrep
+%endmacro
 
 section .text
     mov ah, 00h     ;--------------------------------
     mov al, 13h     ; set screen 320x200 256colours
     int 10h         ;--------------------------------
 
-    call clearScreen
-    call initViewport
-
-    call initTimer
+    init:
+        call initLevel
 
     gameLoop:
+        cmp byte [MAZE_AMOUNT_OF_GUMS], 0
+        je goToNextLevel
+
         call waitForNextFrame
         call pacmanMovement
         call pacmanAnimation
@@ -26,6 +37,53 @@ section .text
         jmp readKeyboard
 
 ; ------------------------------
+    goToNextLevel:
+        inc byte [level]
+        call deallocationViewport
+        call initLevel
+        jmp gameLoop
+        
+    initLevel:
+        ; maze
+        mov bx, ROW_NUMBER * COLUMN_NUMBER
+        .eachCells:
+            mov al, [mazesave+bx]
+            mov [maze+bx], al
+            dec bx
+            jnz .eachCells
+        ; gums
+        mov byte [MAZE_AMOUNT_OF_GUMS], 246
+        ; pacman
+        mov word [pacman_x_pos], PACMAN_INITIAL_X_POS
+        mov word [pacman_y_pos], PACMAN_INITIAL_Y_POS
+        mov word [pacman_x_speed], 2
+        mov word [pacman_y_speed], 0
+        mov word [pacman_x_speed_buffer], 0
+        mov word [pacman_y_speed_buffer], 0
+        mov byte [movement_buffered], 0
+        ; ghosts
+        mov word [ghost_x_pos], BLINKY_INITIAL_X_POS
+        mov word [ghost_x_pos+2], PINKY_INITIAL_X_POS
+        mov word [ghost_x_pos+4], INKY_INITIAL_X_POS
+        mov word [ghost_x_pos+6], CLYDE_INITIAL_X_POS
+        mov word [ghost_y_pos], BLINKY_INITIAL_Y_POS
+        mov word [ghost_y_pos+2], PINKY_INITIAL_Y_POS
+        mov word [ghost_y_pos+4], INKY_INITIAL_Y_POS
+        mov word [ghost_y_pos+6], CLYDE_INITIAL_Y_POS
+
+        mov byte [cage_amount_of_ghosts], 3
+        mov byte [ghost_waiting_in_cage], 0 
+        mov byte [ghost_waiting_in_cage+1], 1
+        mov byte [ghost_waiting_in_cage+2], 1
+        mov byte [ghost_waiting_in_cage+3], 1
+
+        initGhosts
+
+        call clearScreen
+        call initViewport
+        call initTimer
+        ret
+
     waitForNextFrame:
         ; will not stay like this, it works for now by being kind of a 30 fps loop, but it is juts a sleep function, not even dynamic
         mov dx, 10000
