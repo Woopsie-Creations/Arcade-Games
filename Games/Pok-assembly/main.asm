1b146429fe5fb94715938d036ca3e377a66dd294
EnzoGuillouche <145991192+EnzoGuillouche@users.noreply.github.com>
2025-06-05 21:22:31 +0200

    INIT code for Pokassembly yipie (load sprites, viewport)

---
+++ Games/Pok-assembly/main.asm
@@ -0,0 +1,38 @@
+org 100h
+
+%define TRUE 1
+%define FALSE 0
+
+%include "Sprites/include_sprites.inc"
+%include "Variables/include_variables.inc"
+
+section .data
+    poke db "Sprites/BULBASAF.bin", 0
+    poke2 db "Sprites/BULBASAB.bin", 0
+
+section .text
+    mov ah, 00h     ;--------------------------------
+    mov al, 13h     ; set screen 320x200 256 colors
+    int 10h         ;--------------------------------
+    call initViewport
+
+    gameLoop:
+        call displayFrame
+        jmp gameLoop
+
+; ------------------------------
+    resetRegisters:
+        xor ax, ax
+        xor bx, bx
+        xor cx, cx
+        xor dx, dx
+        ret
+
+    exitProgram:
+        call deallocationViewport
+        mov ah, 4Ch
+        xor al, al
+        int 21h
+
+%include "loadFromFile.inc"
+%include "display.inc"
---
+++ Games/Pok-assembly/startPi.sh
@@ -0,0 +1,26 @@
+clear
+
+# Set up paths
+ROOT_DIR="$(dirname "$(realpath "$0")")"
+ROOT_DIR="$(dirname "$ROOT_DIR")"
+NASM="nasm"  # Assumes NASM is installed globally
+GAME_DIR="$ROOT_DIR/Pok-assembly"
+GAME_BIN="$GAME_DIR/Bin"
+DOSBOX_BIN="/usr/bin/dosbox"
+DOSBOX_CONF="$HOME/.dosbox/dosbox-0.74.conf"
+
+# Assemble the .asm sprites
+rm -rf "$GAME_BIN/Sprites/"*
+rm -rf "$GAME_BIN/Sprites/".* 2>/dev/null
+
+find "$GAME_DIR/Sprites" -type f -name '*.asm' | while read -r asm_file; do
+    filename=$(basename "$asm_file" .asm)
+    output_file="$GAME_BIN/Sprites/${filename}.bin"
+    "$NASM" "$asm_file" -f bin -o "$output_file"
+done
+
+# Assemble the game
+"$NASM" "$GAME_DIR/main.asm" -f bin -o "$GAME_BIN/PA.com"
+
+# Run the game with the explicit config
+"$DOSBOX_BIN" -c "MOUNT c $GAME_BIN" -c "c:" -c "loadhigh PA.com" -c "exit"
